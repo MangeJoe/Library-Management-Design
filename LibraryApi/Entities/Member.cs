d4e45694355ed45377a80d4dcc1e17e0b00227d6
Mange Joe <madzira3@gmail.com>
2026-05-21 16:27:04 +0200

    Add project files.

---
+++ LibraryApi/Entities/Member.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibraryApi.Entities
+{
+    public class Member
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("userID")]
+        public int UserID { get; set; }
+
+        [Column("membership_id")]
+        public string Membership_id { get; set; } = string.Empty;
+
+        [Column("address")]
+        public string Address { get; set; } = string.Empty;
+
+        [Column("borrow_limit")]
+        public int Borrow_Limit { get; set; } 
+
+        [Column("status")]
+        public string Status { get; set; } = string.Empty;
+    }
+}
---
+++ LibraryApi/Entities/User.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibraryApi.Entities
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("userID")]
+        public int UserID { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; }=string.Empty;
+
+        [Column("phone_number")]
+        public string Phone_Number { get; set; }= string.Empty;
+
+        [Column("email")]
+        public string Email { get; set; } = string.Empty;
+
+        [Column("password")]
+        public string Password { get; set; }=string.Empty;
+
+        [Column("created_at")]
+        public DateTime CreatedAt { get; set;} = DateTime.Now;
+
+        
+    }
+}
