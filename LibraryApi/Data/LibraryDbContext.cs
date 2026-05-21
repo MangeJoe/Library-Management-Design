5db15d89eeb7eb2888488fd4b96fe7686cc1132e
Mange Joe <madzira3@gmail.com>
2026-05-21 17:19:20 +0200

    added dbSets and overrides the modelCretion method

---
+++ LibraryApi/Data/LibraryDbContext.cs
@@ -0,0 +1,40 @@
+﻿using LibraryApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryApi.Data
+{
+    public class LibraryDbContext:DbContext
+    {
+        public LibraryDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        //each table has a list of entities
+        public DbSet<User> Users { get; set; }
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Librarian> Librarians { get; set; }
+        public DbSet<Manager> Manager { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Member>().ToTable("Member");
+            modelBuilder.Entity<Librarian>().ToTable("Librarian");
+            modelBuilder.Entity<Manager>().ToTable("Manager");
+
+            modelBuilder.Entity<Member>()
+                .Property(m => m.UserID)
+                .HasColumnName("user_Id");
+
+            modelBuilder.Entity<Librarian>()
+                .Property(l => l.UserID)
+                .HasColumnName("user_Id");
+
+
+            modelBuilder.Entity<Manager>()
+                .Property(m => m.UserID)
+                .HasColumnName("user_Id");
+        }
+    }
+}
