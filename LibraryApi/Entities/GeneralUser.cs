269fdf9ab0c905d3ed1523d3811048b3f9c8bf91
Mange Joe <madzira3@gmail.com>
2026-05-22 17:39:03 +0200

    added general user for managing crud operations easily

---
+++ LibraryApi/Entities/GeneralUser.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibraryApi.Entities
+{
+    public class GeneralUser
+    {
+        public int UserID { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Phone_Number { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime HiredDate { get; set; }
+        public string Shift { get; set; } = string.Empty;
+        public string OfficeLocation { get; set; } = string.Empty;
+        public string Membership_id { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public int Borrow_Limit { get; set; }
+        public string Status { get; set; } = string.Empty;
+    }
+}
--- LibraryApi/Program.cs
+++ LibraryApi/Program.cs
@@ -1,17 +1,28 @@
+using LibraryApi.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Configure the HTTP request pipeline.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+//always add the dbcontext before building the app ,everything that use bulder.Services must be before builder.Build()
+builder.Services.AddDbContext<LibraryDbContext>(x =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    x.UseSqlServer(connectionString);
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
